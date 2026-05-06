7641b33ede24ae70e41380c1dc484fe29e76e1ac
Michael Anzengruber <m.anzengruber@gmx.net>
2026-05-06 08:01:01 +0200

    first C hello world

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Mein Name ist Michi.\n");
+    printf("Ich lerne gerade C.\n");
+    printf("Das ist meine erstes echtes Training.\n");
+
+    return 0;
+}
